e90441d000cdcd2ccd9bf3e5317b0ff071c01b7e
Everett Maus <evmaus@microsoft.com>
2019-03-21 16:12:08 -0700

    Final fix to set up cluster

--- scripts/set-up-cluster.sh
+++ scripts/set-up-cluster.sh
@@ -4,6 +4,6 @@
 ../bazel-bin/src/clustersat/leader/leader_server --listening_address=0.0.0.0:50051 \
    --node_addresses="localhost:50052" --logtostderr=1 &
 ../bazel-bin/src/clustersat/node/tribblesat_server --listening_address=0.0.0.0:50052 --configuration=default &
-#../bazel-bin/src/clustersat/node/tribblesat_server --listening_address=0.0.0.0:50053 --configuration=linear &
+../bazel-bin/src/clustersat/node/tribblesat_server --listening_address=0.0.0.0:50053 --configuration=linear &
 
 wait
